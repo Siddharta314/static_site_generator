71aba409d9f39896907ced5d9634c7f777361d41
Emilio Sala <emilio_k88@hotmail.com>
2026-04-23 10:35:48 +0200

    FIX: dest file to docs/

--- src/main.py
+++ src/main.py
@@ -9,10 +9,11 @@ def main():
     if len(sys.argv) > 1:
         base_path = sys.argv[1]
     src_path = "./static"
-    dest_path = "./public"
+    dest_path = "./docs"
     safe_delete_directory(dest_path)
     copy_recursive_directory(src_path, dest_path)
-    generate_pages_recursive("content/", "template.html", "docs/", base_path)
+    generate_pages_recursive("content/", "template.html", dest_path, base_path)
+
 
 
 if __name__ == "__main__":
